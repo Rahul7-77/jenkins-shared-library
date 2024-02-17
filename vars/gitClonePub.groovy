37a92cfd86ebc810ffd7c76d58621ca1d389282f
Rahul7_77 <rahuldasari7502@gmail.com>
2024-02-17 12:28:30 +0530

    Temporarily removed git class fike

--- vars/gitClonePub.groovy
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env groovy
-import com.example.GT
-
-def call(String branch = 'main', boolean changelog = false, boolean poll = false, String url) {
-    // Clone the Git repository with specified configuration options
-    git branch: branch, changelog: changelog, poll: poll, url: url
-}
